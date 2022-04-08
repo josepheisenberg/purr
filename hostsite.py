09d29fc368f2efa4c0712449890f2cda4cc1a3d8
jeisenbe@purdue.edu <jeisenbe@purdue.edu>
2022-04-08 02:11:39 -0400

    Changes

--- hostsite.py
+++ hostsite.py
@@ -1,6 +1,3 @@
-import streamlit.components.v1 as comps
-import pyvis
+import webbrowser
 
-purrnet = open("C:/CS/ILSResearch/purr/purrnet.html", 'r', encoding = 'utf-8')
-code = purrnet.read()
-comps.html(code)
+webbrowser.open_new_tab("purrnet.html")
