d85e0848fc075476fb2bcbd487e544e4653fb078
josepheisenberg <90732065+josepheisenberg@users.noreply.github.com>
2022-04-07 23:31:48 -0400

    Update hostsite.py

--- hostsite.py
+++ hostsite.py
@@ -1,6 +1,6 @@
 import streamlit.components.v1 as comps
 import pyvis
 
-purrnet = open("purrnet.html", 'r', encoding = 'utf-8')
+purrnet = open("C:/CS/ILSResearch/purr/purrnet.html", 'r', encoding = 'utf-8')
 code = purrnet.read()
 comps.html(code)
